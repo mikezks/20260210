32d998207f84c3d9d22ca08c2fb1f2862e6db468
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2026-02-11 13:55:03 +0100

    demo: injection ctx, take until destroyed, destroy ref, effect in injection ctx

--- libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
+++ libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, effect, signal, untracked } from '@angular/core';
+import { Component, computed, effect, inject, Injector, signal, untracked } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Flight } from '../../logic-flight/model/flight';
 import { injectTicketsFacade } from '../../logic-flight/state/facade';
@@ -19,6 +19,7 @@ import { FlightFilterComponent } from '../../ui-flight/flight-filter/flight-filt
 })
 export class FlightSearchComponent {
   private ticketsFacade = injectTicketsFacade();
+  private readonly injector = inject(Injector);
 
   protected filter = signal({
     from: 'Paris',
@@ -35,7 +36,6 @@ export class FlightSearchComponent {
   protected flights$ = this.ticketsFacade.flights$;
 
   constructor() {
-    effect(() => console.log(this.route()));
     effect(() => {
       this.filter();
       untracked(() => this.search());      
@@ -43,6 +43,10 @@ export class FlightSearchComponent {
   }
 
   protected search(): void {
+    effect(() => console.log(this.route()), {
+      injector: this.injector
+    });
+
     if (!this.filter().from || !this.filter().to) {
       return;
     }
