922045610b42a5be57f8247bc4c9bdca13f773c9
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2026-02-11 13:33:37 +0100

    refactor: generic rxjs interop fn

--- libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,10 +1,10 @@
-import { Component, effect, inject, input, numberAttribute, signal } from '@angular/core';
-import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { Component, effect, inject, input, numberAttribute } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { validatePassengerStatus } from '../../util-validation/passenger-validator/passenger-status.validator';
-import { initialPassenger } from '../../logic-passenger/model/passenger';
+import { signalOperators } from '@flight-demo/shared/core';
+import { pipe, switchMap } from 'rxjs';
 import { PassengerService } from '../../logic-passenger/data-access/passenger.service';
-import { switchMap } from 'rxjs';
+import { initialPassenger } from '../../logic-passenger/model/passenger';
+import { validatePassengerStatus } from '../../util-validation/passenger-validator/passenger-status.validator';
 
 
 @Component({
@@ -27,12 +27,10 @@ export class PassengerEditComponent {
   });
 
   readonly id = input(0, { transform: numberAttribute });
-  private readonly passenger = toSignal(
-    toObservable(this.id).pipe(
-      switchMap(id => this.passengerService.findById(id)
-    )
-  ), { initialValue: initialPassenger });
-
+  private readonly passenger = signalOperators(this.id, pipe(
+    switchMap(id => this.passengerService.findById(id))
+  ), initialPassenger);
+  
   constructor() {
     effect(() => console.log(this.id()));
     effect(() => this.editForm.patchValue(this.passenger()));
