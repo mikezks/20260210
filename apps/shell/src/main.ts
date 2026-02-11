8420815784c649c608e688922ca3d1b4a6a0cf8b
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2026-02-11 10:15:45 +0100

    refactor: schematics bootstrapping

--- apps/shell/src/main.ts
+++ apps/shell/src/main.ts
@@ -1,6 +1,17 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModule } from './app/app.module';
+import { platformBrowser, BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app/app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { routerFeature } from './app/shared/logic-router-state';
+import { UiCoreModule } from './app/shared/ui-core/ui-core.module';
+import { SharedModule } from './app/shared/shared.module';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
 
 
-platformBrowser().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [importProvidersFrom(BrowserModule, AppRoutingModule, HttpClientModule, StoreModule.forRoot(), EffectsModule.forRoot(), StoreModule.forFeature(routerFeature), UiCoreModule, SharedModule)]
+})
   .catch(err => console.error(err));
