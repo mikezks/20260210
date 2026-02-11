bedfa2edf7611b9a3434021f2be317af029b9792
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2026-02-11 09:52:17 +0100

    refactor: ui cmp to standalone

--- apps/shell/src/app/booking/ui-flight/flight-card/flight-card.component.ts
+++ apps/shell/src/app/booking/ui-flight/flight-card/flight-card.component.ts
@@ -1,10 +1,15 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { Flight } from '../../logic-flight';
+import { DatePipe, NgStyle } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-flight-card',
-  standalone: false,
+  imports: [
+    NgStyle, DatePipe,
+    RouterLink
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <div
