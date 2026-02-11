7764547db114c5a363d4d2f8e0be750aec6cd6d9
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2026-02-11 15:56:02 +0100

    demo: zoneless missing sync

--- libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
+++ libs/domain/booking/src/lib/feature-flight/flight-search/flight-search.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, effect, signal, untracked } from '@angular/core';
+import { ChangeDetectorRef, Component, computed, effect, inject, signal, untracked } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Flight } from '../../logic-flight/model/flight';
 import { injectTicketsFacade } from '../../logic-flight/state/facade';
@@ -19,6 +19,7 @@ import { FlightFilterComponent } from '../../ui-flight/flight-filter/flight-filt
 })
 export class FlightSearchComponent {
   private ticketsFacade = injectTicketsFacade();
+  private readonly cdRef = inject(ChangeDetectorRef);
 
   protected filter = signal({
     from: 'Paris',
@@ -33,6 +34,7 @@ export class FlightSearchComponent {
     5: true
   };
   protected flights = this.ticketsFacade.flights;
+  protected firstname = 'Mary';
 
   constructor() {
     effect(() => console.log(this.route()));
@@ -40,6 +42,11 @@ export class FlightSearchComponent {
       this.filter();
       untracked(() => this.search());      
     });
+    setTimeout(() => {
+      this.firstname = 'Peter';
+      // this.cdRef.markForCheck();
+      console.log(this.firstname);
+    }, 5_000);
   }
 
   protected search(): void {
